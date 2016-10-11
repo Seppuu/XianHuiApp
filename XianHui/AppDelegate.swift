7b3b8852ea0db8bc58b1b2d10915f3279f31842e
Seppuu <Dan@jidanyudemacbook-pro.local>
2016-10-11 15:56:28 +0800

    1.0.0

--- XianHui/AppDelegate.swift
+++ XianHui/AppDelegate.swift
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        LCChatKit.sharedInstance().useDevPushCerticate = true
+        LCChatKit.sharedInstance().useDevPushCerticate = false
         ChatKitExample.invokeThisMethodInDidFinishLaunching()
         //ios 10 兼容
         replyPushNotificationAuthorization(application)
